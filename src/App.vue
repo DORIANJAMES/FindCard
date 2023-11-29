1b09e60b5c104aa0a09dfc130a016e4bfa694572
Dorian JAMES <dorianthelycan@gmail.com>
2023-11-29 15:33:58 +0300

    Transition animation added on Celebrate and Failure Components.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container mt-5">
-    <component :is="activeComponent" @componentInfo="changeComponent($event)"></component>
+    <transition name="slideContainer" mode="out-in">
+      <component :is="activeComponent" @componentInfo="changeComponent($event)"></component>
+    </transition>
   </div>
 </template>
 <script>
@@ -9,15 +11,15 @@ import Failure from "./components/Failure.vue";
 import Celebrate from "./components/Celebrate.vue";
 
 export default {
-  name:"App",
-  components:{GameCards,Failure,Celebrate},
-  data(){
-    return{
-      activeComponent:'GameCards',
+  name: "App",
+  components: {GameCards, Failure, Celebrate},
+  data() {
+    return {
+      activeComponent: 'GameCards',
     }
   },
-  methods:{
-    changeComponent(componentName){
+  methods: {
+    changeComponent(componentName) {
       console.log(componentName);
       this.activeComponent = componentName;
     }
@@ -26,7 +28,37 @@ export default {
 }
 </script>
 <style>
-body{
+body {
   font-family: sans-serif;
+}
+
+.slideContainer-enter{}
+.slideContainer-enter-active{
+  animation: slideIn .3s ease-in-out forwards;
+}
+.slideContainer-leave{}
+.slideContainer-leave-active{
+  animation: slideOut .3s ease-in-out backwards;
+}
+@keyframes slideIn {
+  from {
+    transform: translateX(-1000px);
+    opacity: 0;
+  }
+  to {
+    transform: translateX(0px);
+    opacity: 1;
+  }
+}
+
+@keyframes slideOut {
+  from {
+    transform: translateX(0px);
+    opacity: 1;
+  }
+  to {
+    transform: translateX(1000px);
+    opacity: 0;
+  }
 }
 </style>
